b26073c71223e89eeefaa4f3c31fb13d221e17a0
Bruce Zhang (RD-CN) <bruce_zhang@trendmicro.com>
2020-10-21 10:13:12 +0800

    customize theme

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-button type="primary">hello world</a-button>
   </div>
 </template>
 
